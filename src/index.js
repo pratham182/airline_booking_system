1e2c359e9d190fa840c45bfbfaa494ea4556b659
pratham182 <arorapratham900@gmail.com>
2023-11-13 04:54:54 +0530

    added city_repo and unique constraints

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const express=require("express");
 const bodyParser=require("body-parser");
 
 const {port}=require("./config/serverConfig");
-
+const {City}=require("./models/index");
+const city_repo=require("./repository/city-repository");
+//const city = require("./models/city");
 
 const setupAndStartServer=async()=>{
     
@@ -14,11 +16,17 @@ const setupAndStartServer=async()=>{
     app.use(bodyParser.urlencoded({
         extended:true
     }))
+   
 
 
-    app.listen(port,()=>{
+    app.listen(port,async()=>{
+
         console.log(`App is listening on port ${port}`);
-         
+        //  const repo=new city_repo();
+        //  repo.createCity({
+        //     name:"Shyam"
+        //  });
+    
     });
 }
 setupAndStartServer();
